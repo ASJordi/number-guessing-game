d02538cdef4a3e642737022b47d37687b34c6b2b
Jordi Ayala <jord7as@gmail.com>
2024-11-15 19:32:54 -0600

    refactor: Update class to use OOP

--- src/main/java/dev/asjordi/Main.java
+++ src/main/java/dev/asjordi/Main.java
@@ -5,23 +5,29 @@ import java.util.Scanner;
 
 public class Main {
     
-	private static Scanner sc = new Scanner(System.in);
+	private final Scanner sc = new Scanner(System.in);
+	private final Random r = new Random();
 	
 	public static void main(String[] args) {
-        Random r = new Random();
-    	int currentAttempts = 0;
-        int secretNumber = r.nextInt(1, 101);
+		Main m = new Main();
+    	m.play();
+    }
+	
+	public void play() {
+		int currentAttempts = 0;
+        int secretNumber = generateNumber();
         
         welcome();
+        
         int level = getLevel();
         int attempts = level == 1 ? 10 : level == 2 ? 5 : 3; 
         System.out.println("Great! You have selected the " + (level == 1 ? "Easy" : level == 2 ? "Medium" : "Hard") + " difficulty level.");
-        System.out.println("Let's start the game!");
+        System.out.println("Let's start the game!\n");
         
         while (attempts > 0) {
         	System.out.println("Enter your guess:");
         	String input = sc.nextLine();
-        	input = input.replace(" ", "").trim();
+        	input = cleanString(input);
         	int userNumber = Integer.parseInt(input);
         	currentAttempts++;
         	
@@ -29,7 +35,7 @@ public class Main {
 				System.out.println("Congratulations! You guessed the correct number in " + currentAttempts +" attempts.");
 				break;
 			} else {
-				System.out.println("The secret number is " + (userNumber > secretNumber ? "less" : "greater"));
+				System.out.println("The secret number is " + (userNumber > secretNumber ? "less" : "greater") + " than " + userNumber);
 				attempts--;
 			}
         	
@@ -37,9 +43,9 @@ public class Main {
         		System.out.println("You lost! The secret number was " + secretNumber);
         	}
 		}
-    }
+	}
 	
-	private static int getLevel() {
+	private int getLevel() {
 		boolean valid = false;
 		int level = -1;
 		String validLevels = "123";
@@ -52,7 +58,7 @@ public class Main {
 	        System.out.println("Enter your choice: ");
 	        
 	        String input = sc.nextLine();
-	        input = input.replace(" ", "").trim();
+	        input = cleanString(input);
 	        
 	        if (!validLevels.contains(input)) continue;
 	        
@@ -63,10 +69,18 @@ public class Main {
 		return level;
 	}
 	
+	private int generateNumber() {
+		return this.r.nextInt(1, 101);
+	}
 	
-	private static void welcome() {
+	
+	private void welcome() {
 		System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("I'm thinking of a number between 1 and 100.");
-        System.out.println("You have 5 chances to guess the correct number.\n");
+        System.out.println("You have N chances to guess the correct number.\n");
+	}
+	
+	private String cleanString(String str) {
+		return str.replace(" ", "").trim();
 	}
 }
